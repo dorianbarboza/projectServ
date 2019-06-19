1724245a56c4097da8a79b9431b0342de76d9af6
dorianbarboza <dorianbarboza@gmail.com>
2019-06-19 15:19:24 -0500

    2. Script Database

--- script.sql
+++ script.sql
@@ -1,7 +1,12 @@
 CREATE DATABASE project;
 
+USE project;
+
 CREATE TABLE Usuario(
 ID_Usuario int primary key not null auto_increment,
-Username varchar(10),
+Username varchar(20),
 Password varchar(10)
 );
+
+INSERT INTO Usuario (Username, Password)
+VALUES ('dorianbarboza','123');
