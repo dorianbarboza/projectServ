7a246fe9c595cc2965822117d9af42eb218e0d4c
dorianbarboza <dorianbarboza@gmail.com>
2019-06-21 00:39:29 -0500

    2. Coneccion con mysql

--- ms/code.py
+++ ms/code.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pymysql
 
-def main():
-    print "Funcion Main"
-    control()
+############### CONFIGURAR ESTO ###################
+# Abre conexion con la base de datos
+db = pymysql.connect("localhost","root","","project")
+##################################################
 
-def control():
-    print "Funcion Control"
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
 
-if __name__=='__main__':
-    main()
+# ejecuta el SQL query usando el metodo execute().
+cursor.execute("SELECT VERSION()")
 
+# procesa una unica linea usando el metodo fetchone().
+data = cursor.fetchone()
+print ("Database version : {0}".format(data))
+
+# desconecta del servidor
+db.close()
