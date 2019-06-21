3750ed2768f03f7a6a87e1df7e30bb9e58c58ad7
dorianbarboza <dorianbarboza@gmail.com>
2019-06-20 23:52:03 -0500

    1. Iniciando con python

---
+++ ms/code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    print "Funcion Main"
+    control()
+
+def control():
+    print "Funcion Control"
+
+if __name__=='__main__':
+    main()
+
