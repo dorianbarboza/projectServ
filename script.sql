59cadc736ccb7603a68e9bd99a5455056068d7d9
dorianbarboza <dorianbarboza@gmail.com>
2019-06-22 21:30:11 -0500

    3. Añadiendo tablas artista, album, cancion

--- script.sql
+++ script.sql
@@ -2,6 +2,7 @@ CREATE DATABASE project;
 
 USE project;
 
+-- Tabla Usuario
 CREATE TABLE Usuario(
 ID_Usuario int primary key not null auto_increment,
 Username varchar(20),
@@ -10,3 +11,43 @@ Password varchar(10)
 
 INSERT INTO Usuario (Username, Password)
 VALUES ('dorianbarboza','123');
+
+-- Tabla Artista
+CREATE TABLE Artista(
+ID_Artista int primary key not null,
+Seudonimo varchar(30),
+Nombre varchar(30),
+Apellido varchar(30),
+FechaNacimiento date,
+Ciudad varchar(30),
+Pais varchar(30),
+UrlImg varchar(100)
+);
+-- Tabla Album
+CREATE TABLE Album(
+ID_Album int primary key not null,
+NombreAlbum varchar(30),
+Publicacion date,
+CiudadGrabacion varchar(30),
+PaisGrabacion varchar(30),
+Duracion time,
+Genero varchar(30),
+UrlImgAlbum varchar(100)
+-- Fk Artista
+ALTER TABLE Album
+ADD ID_Artista int,
+ADD FOREIGN KEY (ID_Artista)
+REFERENCES Artista (ID_Artista);
+);
+-- Tabla Cancion
+CREATE TABLE Cancion(
+ID_Cancion int primary key not null,
+NombreCancion varchar(30),
+DuracionCancion time,
+UrlSong varchar(100)
+-- FK Album
+ALTER TABLE Cancion
+ADD ID_Album int,
+ADD FOREIGN KEY (ID_Album)
+REFERENCES Album (ID_Album);
+);
