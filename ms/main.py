3a66505621a293689ecde230a42b47295f3102cb
dorianbarboza <dorianbarboza@gmail.com>
2019-06-24 22:07:34 -0500

    7. Listo WS Artista GET por id

--- ms/main.py
+++ ms/main.py
@@ -9,19 +9,24 @@ from flask import flash, request
 from werkzeug import generate_password_hash, check_password_hash
 
 @app.route('/add', methods=['POST'])
-def add_user():
+def add():
 	try:
 		_json = request.json
-		_name = _json['name']
-		_email = _json['email']
-		_password = _json['pwd']
+		_ID_Artista = _json['ID_Artista']
+		_Seudonimo = _json['_Seudonimo']
+		_Nombre = _json['Nombre']
+		_Apellido = _json['Apellido']
+		_FechaNacimiento = _json['FechaNacimiento']
+		_Ciudad = _json['Ciudad']
+		_Pais = _json['Pais']
+		_UrlImg = _json['UrlImg']
 		# validate the received values
-		if _name and _email and _password and request.method == 'POST':
+		if _ID_Artista and _Seudonimo and _Nombre and _Apellido and _FechaNacimiento and _Ciudad and _Pais and _UrlImg and request.method == 'POST':
 			#do not save password as a plain text
-			_hashed_password = generate_password_hash(_password)
+			#_hashed_password = generate_password_hash(_password)
 			# save edits
-			sql = "INSERT INTO tbl_user(user_name, user_email, user_password) VALUES(%s, %s, %s)"
-			data = (_name, _email, _hashed_password,)
+			sql = "INSERT INTO Artista (ID_Artista, Seudonimo, Nombre, Apellido, FechaNacimiento, Ciudad, Pais, UrlImg) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+			data = (_ID_Artista, _Seudonimo, _Nombre, _Apellido, _FechaNacimiento, _Ciudad, _Pais, _UrlImg ,)
 			conn = mysql.connect()
 			cursor = conn.cursor()
 			cursor.execute(sql, data)
@@ -53,12 +58,12 @@ def getAllArtista():
 		cursor.close()
 		conn.close()
 
-@app.route('/user/<int:id>')
-def user(id):
+@app.route('/user/<int:id_artista>')
+def user(id_artista):
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute("SELECT user_id id, user_name name, user_email email, user_password pwd FROM tbl_user WHERE user_id=%s", id)
+		cursor.execute("SELECT ID_Artista id_artista, Seudonimo seudonimo, Nombre nombre, Apellido apellido, FechaNacimiento fechanacimiento, Ciudad ciudad, Pais pais, UrlImg urlimg FROM Artista WHERE ID_Artista=%s", id_artista)
 		row = cursor.fetchone()
 		resp = jsonify(row)
 		resp.status_code = 200
@@ -70,20 +75,24 @@ def user(id):
 		conn.close()
 
 @app.route('/update', methods=['PUT'])
-def update_user():
+def update():
 	try:
 		_json = request.json
-		_id = _json['id']
-		_name = _json['name']
-		_email = _json['email']
-		_password = _json['pwd']
+		_ID_Artista = _json['ID_Artista']
+		_Seudonimo = _json['_Seudonimo']
+		_Nombre = _json['Nombre']
+		_Apellido = _json['Apellido']
+		_FechaNacimiento = _json['FechaNacimiento']
+		_Ciudad = _json['Ciudad']
+		_Pais = _json['Pais']
+		_UrlImg = _json['UrlImg']
 		# validate the received values
-		if _name and _email and _password and _id and request.method == 'PUT':
+		if _ID_Artista and _Seudonimo and _Nombre and _Apellido and _FechaNacimiento and _Ciudad and _Pais and _UrlImg and request.method == 'PUT':
 			#do not save password as a plain text
-			_hashed_password = generate_password_hash(_password)
+			#_hashed_password = generate_password_hash(_password)
 			# save edits
-			sql = "UPDATE tbl_user SET user_name=%s, user_email=%s, user_password=%s WHERE user_id=%s"
-			data = (_name, _email, _hashed_password, _id,)
+			sql = "UPDATE Artista SET Seudonimo=%s, Nombre=%s, Apellido=%s, FechaNacimiento=%s, Ciudad=%s, Pais=%s, UrlImg=%s WHERE ID_Artista=%s"
+			data = (_ID_Artista, _Seudonimo, _Nombre, _Apellido, _FechaNacimiento, _Ciudad, _Pais, _UrlImg, ID_Artista,)
 			conn = mysql.connect()
 			cursor = conn.cursor()
 			cursor.execute(sql, data)
